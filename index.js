f4ce56172f0c03bcd53c2160249da10497c882a8
erik <erik168@163.com>
2014-03-22 23:18:52 +0800

    export something

---
+++ index.js
@@ -0,0 +1,12 @@
+exports.html = {
+    lint: require('./lib/html/htmlhint').lint,
+    config: require('./lib/html/config')
+};
+
+exports.css = {
+    config: require('./lib/css/config')
+};
+
+exports.js = {
+    config: require('./lib/js/config')
+};
