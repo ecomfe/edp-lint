14b9e9245503cfb25ab3a071dce33bb8560c0792
chris <wfsr@foxmail.com>
2015-04-16 20:35:41 +0800

    brige to fecs

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-exports.html = {
-    lint: require('./lib/html/htmlhint').lint,
-    config: require('./lib/html/config')
-};
+module.exports = require('fecs');
 
-exports.css = {
-    config: require('./lib/css/config')
-};
-
-exports.js = {
-    config: require('./lib/js/config'),
-    jscsConfig: require('./lib/js/jscsrc.json')
-};
