47a1f53f5011eec4a9f06c6e34a4c0177f106c6c
chris <wfsr@foxmail.com>
2015-04-17 16:41:57 +0800

    update test case

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,18 +1,18 @@
 /***************************************************************************
- * 
+ *
  * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
- * $Id$ 
- * 
+ * $Id$
+ *
  **************************************************************************/
- 
- 
- 
+
+
+
 /**
  * util.spec.js ~ 2014/03/28 15:57:24
  * @author leeight(liyubei@baidu.com)
- * @version $Revision$ 
- * @description 
- *  
+ * @version $Revision$
+ * @description
+ *
  **/
 
 var path = require('path');
@@ -20,16 +20,20 @@ var path = require('path');
 var util = require('../lib/util');
 
 describe('util', function(){
-    it('getIgnorePatterns', function() {
-        var file = path.join(__dirname, 'data', '.jshintignore');
-        var patterns = util.getIgnorePatterns(file);
+    it('getCommandName', function() {
+        var name = util.getCommandName();
 
-        expect(patterns).toEqual([ 'a/**', 'b', 'c/**', 'd' ]);
+        expect(name).toEqual('jasmine-node');
     });
 
-    it('isIgnored', function(){
-        process.chdir(path.join(__dirname, '..'));
-        expect(util.isIgnored('lib/js/config.js')).toBe(true);
+    it('getFecsCheckOptions: no-type', function(){
+        var options = util.getFecsCheckOptions();
+        expect(options.indexOf('type:') > -1).toBe(false);
+    });
+
+    it('getFecsCheckOptions: hasType', function(){
+        var options = util.getFecsCheckOptions(true);
+        expect(options.indexOf('type:') > -1).toBe(true);
     });
 });
 
